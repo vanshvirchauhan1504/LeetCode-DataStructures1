6f1f6241d39270e6e00302f241e57a3656b30eff
VanshChauhan15 <97740365+VanshChauhan15@users.noreply.github.com>
2022-06-05 20:12:49 +0530

    first commit

---
+++ 1.two-sum.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] x=new int[2];
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    x[0]=i;
+                    x[1]=j;
+                    break;
+                }
+            }
+        }
+        return x;
+    }
+}
+// @lc code=end
+
